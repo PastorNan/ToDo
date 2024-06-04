8941b39434e5e9f1d9b0940f88f407aff68be3a1
PastorNan <ronan.eastmond@gmail.com>
2024-06-04 15:44:57 -0400

    front end

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,23 @@ import ShowAllTask from './Components/ShowAllTask'
 import AddTask from './Components/AddTask'
 import UpdateTask from './Components/UpdateTask'
 import TaskNotFound from './Components/TaskNotFound'
+import Login from './Components/Login'
 
 function App() {
 
-  return (
+  return ( 
+    
     <>
       <h1 className='text-black  text-6xl text-center'>TO DO LIST </h1>
       <Routes>
-        <Route path='/' element={<ShowAllTask/>}/>
-        <Route path='/task'>
-          <Route path='add' element={<AddTask/>} />
-          <Route path='update/:id' element={<UpdateTask />} />
-        </Route>
+          <Route path='/' element={<Login />} />
+        <Route path='/view' element={<ShowAllTask/>}/>
+          <Route path='/add' element={<AddTask/>} />
+          <Route path='/update' element={<UpdateTask />} />
        <Route path='*' element={<TaskNotFound/>} />
       </Routes>
-    </>
-  )
+    </> 
+  ) 
 }
 
 export default App
--- src/Components/AddTask.jsx
+++ src/Components/AddTask.jsx
@@ -9,10 +9,10 @@ const AddTask = () => {
         dueDate: "",
     })
 
-    const addABook = (e) => {
+    const addATask = (e) => {
         e.preventDefault()
 
-        //console.debug(formData)
+        console.debug(formData)
         const req = new Request(
             'http://localhost:3000/add', {
             method: "POST",
@@ -60,16 +60,16 @@ const AddTask = () => {
                     <form className="max-w-md mx-auto">
 
                         <div className="relative z-0 w-full mb-5 group">
-                            <input type="title" name="title" id="title" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required onChange={handleChange} />
-                            <label htmlFor="title" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">title</label>
+                            <input type="task" name="task" id="task" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" required onChange={handleChange} />
+                            <label htmlFor="task" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">task</label>
                         </div>
                         <div className="relative z-0 w-full mb-5 group">
-                            <input type="author" name="author" id="author" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required onChange={handleChange} />
-                            <label htmlFor="author" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">author</label>
+                            <input type="owner" name="owner" id="owner" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required onChange={handleChange} />
+                            <label htmlFor="owner" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">owner</label>
                         </div>
                         <div className="relative z-0 w-full mb-5 group">
-                            <input type="price" name="price" id="price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " onChange={handleChange} />
-                            <label htmlFor="price" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">price</label>
+                            <input type="dueDate" name="dueDate" id="dueDate" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " onChange={handleChange} />
+                            <label htmlFor="dueDate" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">dueDate</label>
                         </div>
 
 
